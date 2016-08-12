0e34b970377b3096b6c564f8c97769107ff466ae
John Pallister <john@synchromesh.com>
2016-08-12 15:09:52 +0100

    Revert "Added timeouts for LispWorks."
    This reverts commit 72124389fbc53ba77e79ca7c1ab93dad61680a5c.

--- set-timeouts.lisp
+++ set-timeouts.lisp
@@ -67,11 +67,5 @@ set."
   #+:cmu
   (setf (lisp::fd-stream-timeout (usocket:socket-stream usocket))
         (coerce read-timeout 'integer))
-  #+lispworks
-  (when read-timeout
-    (setf (stream:stream-read-timeout (usocket:socket-stream usocket)) read-timeout))
-  #+lispworks
-  (when write-timeout
-    (setf (stream:stream-write-timeout (usocket:socket-stream usocket)) write-timeout))
-  #-(or :clisp :ecl :openmcl :sbcl :cmu :lispworks)
+  #-(or :clisp :ecl :openmcl :sbcl :cmu)
   (not-implemented 'set-timeouts))
